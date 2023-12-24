6afb64fdc7ae5da2745bad191701fe3b22f926fe
jasonhung <hello@ifhavebugcallme.com>
2023-12-24 23:35:33 +0800

    commit all

--- phw11-1-iosSdkController/ViewController.swift
+++ phw11-1-iosSdkController/ViewController.swift
@@ -7,13 +7,183 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+@objc protocol CallbackProtocal{
+    @objc func callbackString(String:String)
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var vc2SayLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func present01(_ sender: UIButton) {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = UIColor.green
+        present(viewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func persent02_fullScreen(_ sender: Any) {
+        let viewController2 = ViewController2()
+        viewController2.modalPresentationStyle = .fullScreen
+        viewController2.modalTransitionStyle = .coverVertical
+        present(viewController2, animated: true, completion: {
+            viewController2.textField.isHidden = true
+            viewController2.returnStringDelegateButton.isHidden = true
+        })
+    }
+    
+    
+    @IBAction func otherSetting(_ sender: UIButton) {
+        let setting = 5
+        let viewController2 = ViewController2()
+        
+        switch setting{
+        case 0:
+            viewController2.modalPresentationStyle = .formSheet
+            viewController2.modalTransitionStyle = .coverVertical
+        case 1:
+            viewController2.modalPresentationStyle = .formSheet
+            viewController2.modalTransitionStyle = .flipHorizontal
+        case 2:
+            viewController2.modalPresentationStyle = .fullScreen
+            viewController2.view.alpha = 0.8
+            //viewController2.modalTransitionStyle = .crossDissolve
+        case 3:
+            viewController2.modalPresentationStyle = .overFullScreen
+            viewController2.view.alpha = 0.8
+            //viewController2.modalTransitionStyle = .crossDissolve
+        case 4:
+            //viewController2.modalPresentationStyle = .overCurrentContext
+            viewController2.view.alpha = 0.5
+            viewController2.modalTransitionStyle = .crossDissolve
+            
+        case 5:
+            //viewController2.modalPresentationStyle = .overCurrentContext
+            viewController2.view.alpha = 0.5
+            viewController2.modalTransitionStyle = .flipHorizontal
+            
+        default:
+            break
+        }
+        present(viewController2, animated: true, completion: {
+            viewController2.textField.isHidden = true
+            viewController2.returnStringDelegateButton.isHidden = true
+        })    }
+    
+    @IBAction func present03_CustomPresentAnimation(_ sender: Any) {
+        let viewController2 = ViewController2()
+        viewController2.transitioningDelegate = self
+        viewController2.modalPresentationStyle = .custom
+        
+        present(viewController2, animated: true, completion: {
+            viewController2.textField.isHidden = true
+            viewController2.returnStringDelegateButton.isHidden = true
+        })
+    }
+    
+    @IBAction func testCompletion(_ sender: Any) {
+        let viewController2 = ViewController2()
+        present(viewController2, animated: true, completion: {
+            viewController2.textField.text = "輸入你的名字！"
+            viewController2.returnStringDelegateButton.isHidden = true
+
+            
+        })
+        
+    }
+    
+    @IBAction func testCallbackHandler(_ sender: Any) {
+        
+        let viewController2 = ViewController2()
+        
+        // 設定異步回調的 closure
+        viewController2.callbackHandler = { enteredText in
+            // 在這裡處理回傳的內容
+            print("Entered text from ViewController2: \(enteredText)")
+            self.vc2SayLabel.text = enteredText
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                let resultAlert = UIAlertController(title: "回傳的資料", message: enteredText, preferredStyle: .alert)
+                let okResultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                resultAlert.addAction(okResultAction)
+                self.present(resultAlert, animated: true, completion: nil)
+            }
+        }
+        
+        present(viewController2, animated: true, completion: {
+            viewController2.returnStringDelegateButton.isHidden = true
+            viewController2.textField.text = "輸入你的名字！"
+        })
+        
+        
+    }
+    
+    @IBAction func testCallbackProtocal(_ sender: Any) {
+        let viewController2 = ViewController2()
+        viewController2.callbackDelegate = self
+        
+        present(viewController2, animated: true, completion: {
+            viewController2.textField.text = "Delegate test！"
+        })
+    }
+    
+    
+}
+
+
+
+
+class CustomPresentAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 1.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let fromVC = transitionContext.viewController(forKey: .from),
+              let toVC = transitionContext.viewController(forKey: .to) else {
+            return
+        }
+        
+        let containerView = transitionContext.containerView
+        containerView.addSubview(toVC.view)
+        
+        toVC.view.alpha = 0.0
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            toVC.view.alpha = 1.0
+        }) { (_) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+    }
+}
+
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CustomPresentAnimation()
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil // 如果需要自定義dismiss轉場，同樣實現相關代理方法
+    }
+}
+
+
+extension ViewController: CallbackProtocal{
+    func callbackString(String: String) {
+        vc2SayLabel.text = String
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            let resultAlert = UIAlertController(title: "回傳的資料", message: String, preferredStyle: .alert)
+            let okResultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            resultAlert.addAction(okResultAction)
+            self.present(resultAlert, animated: true, completion: nil)
+        }
+    }
+    
+}
--- phw11-1-iosSdkController/ViewController2.swift
+++ phw11-1-iosSdkController/ViewController2.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class ViewController2: UIViewController {
+    var callbackDelegate: CallbackProtocal?
+    
+    // 創建一個回傳資料按鈕
+    let returnStringDelegateButton = UIButton(type: .system)
+    
+    //添加一個用於保存異步回調的屬性
+    // typealias CompletionHandler = (String) -> Void
+    // var completionHandler: CompletionHandler?
+    var callbackHandler: (String) -> Void = { _ in }
+    let textField = UITextField()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 設置背景顏色為其他顏色
+        view.backgroundColor = UIColor.green
+        
+        // 創建一個關閉按鈕
+        let closeButton = UIButton(type: .system)
+        closeButton.setTitle("關閉", for: .normal)
+        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        closeButton.frame = CGRectMake(self.view.frame.maxX-100, 100, 80, 30);
+        
+        // 創建一個 Label
+        let label = UILabel()
+        label.text = "Hello!"
+        label.textColor = UIColor.black
+        label.textAlignment = .center
+        label.frame = CGRect(x: 0, y: closeButton.frame.maxY + 20, width: view.bounds.width, height: 30)
+        
+        // 創建一個 Label
+        textField.text = ""
+        textField.backgroundColor = UIColor.white
+        textField.frame = CGRect(x:80, y: label.frame.maxY + 20, width: view.bounds.width-160, height: 30)
+        
+        // 將按鈕和 Label 添加到模態視圖控制器的視圖
+        view.addSubview(closeButton)
+        view.addSubview(label)
+        view.addSubview(textField)
+        
+        // 創建一個回傳資料按鈕
+        returnStringDelegateButton.setTitle("回傳資料", for: .normal)
+        returnStringDelegateButton.addTarget(self, action: #selector(returnStringButtonTapped), for: .touchUpInside)
+        returnStringDelegateButton.frame = CGRectMake(self.view.frame.maxX-100, textField.frame.maxY + 20, 80, 30);
+        
+        view.addSubview(returnStringDelegateButton)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 關閉按鈕的點擊事件
+    @objc func closeButtonTapped() {
+        callbackHandler(textField.text!)
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
+    // 回傳資料按鈕的點擊事件
+    @objc func returnStringButtonTapped() {
+        // 使用可選鏈接和可選綁定確保 callbackDelegate 是有值的
+        if let delegate = callbackDelegate {
+            delegate.callbackString(String: textField.text!)
+        } else {
+            print("callbackDelegate is nil")
+        }
 
+        dismiss(animated: true, completion: nil)
+    }
 }
